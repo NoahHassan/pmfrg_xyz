8fea1adc13d8bfbf5d2d64de36f289e979a4bc1f
Michele Mesiti <michele.mesiti@kit.edu>
2026-04-24 12:52:29 +0200

    Testing with lower temperature
    - with high enough temperature X and Y are compatible
      between general and xyz.
    - with lower temperature, we see that X is ok, but Y isn't
      - BUT by replacing the props with their signs in addY!
        in both code versions, we see again agreement.
        => there's likely a problem in Props?
    
    (pair debugged with Noah)

--- pmfrg-general-tests/debug.jl
+++ pmfrg-general-tests/debug.jl
@@ -108,12 +108,12 @@ function test_deriv()
     Par_gen = PMFRG_general.Params(
         System,
         N = 4,
-        temp_max = 100.0,
+        temp_max = 1.0,
         temp_min = 10.0,
         accuracy = 1e-4,
     )
     Par_xyz =
-        PMFRG_old.Params(System, N = 4, temp_max = 100.0, temp_min = 10.0, accuracy = 1e-4)
+        PMFRG_old.Params(System, N = 4, temp_max = 1.0, temp_min = 10.0, accuracy = 1e-4)
 
     # Gamma_(index, ki, 0, -wmw1, -wpw1, flavTransform)
     PMFRG_general.TestFRGAt(Par_gen, zeros(System.Npairs, 3, 3), G_gen)
@@ -162,17 +162,32 @@ X_gen[5, :, :, :, :] .- X_xyz[ConvertToXYZIndex(5), :, :, :, :]
 X_xyz_from_general = similar(X_xyz)
 X_xyz_from_general .= 0.0
 
+function comparison(x, y, rel_threshold)
+    if x == 0 && y == 0
+        return false
+    else
+        abs(x - y) / (abs(x) + abs(y)) > rel_threshold
+    end
+end
+
+
 for n = 1:81
     index = ConvertToXYZIndex(n)
     if (index != -1)
         for m = 1:2, ns = 1:4, nt = 1:4, nu = 1:4
             X_xyz_from_general[index, m, ns, nt, nu] = X_gen[n, m, ns, nt, nu]
+            X_xyz_from_general[index+21, m, ns, nt, nu] = X_gen[n+81, m, ns, nt, nu]
 
-            if abs(X_gen[n, m, ns, nt, nu] - X_xyz[index, m, ns, nt, nu] > 1.0e-14)
+
+            if comparison(X_gen[n, m, ns, nt, nu], X_xyz[index, m, ns, nt, nu], 1.0e-9)
                 println("diff (X): ", X_gen[n, m, ns, nt, nu] - X_xyz[index, m, ns, nt, nu])
             end
 
-            if abs(X_gen[n+81, m, ns, nt, nu] - X_xyz[index+21, m, ns, nt, nu] > 1.0e-14)
+            if comparison(
+                X_gen[n+81, m, ns, nt, nu],
+                X_xyz[index+21, m, ns, nt, nu],
+                1.0e-9,
+            )
                 println(
                     "diff (Y): ",
                     X_gen[n+81, m, ns, nt, nu] - X_xyz[index+21, m, ns, nt, nu],
@@ -183,7 +198,60 @@ for n = 1:81
     end
 end
 
-@test sum((X_xyz_from_general .- X_xyz) .^ 2) < 1.0e-10
+@testset begin
+    println("Testing X and Y equality")
+    @test X_xyz_from_general ≈ X_xyz
 
-A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-reshape(A, 3, 3)'
+    println("Testing X equality")
+    @test X_xyz_from_general[1:21, :, :, :, :] ≈ X_xyz[1:21, :, :, :, :]
+
+    println("Testing Y equality")
+    @test X_xyz_from_general[22:42, :, :, :, :] ≈ X_xyz[22:42, :, :, :, :]
+end
+
+
+
+function compare_Y_detailed(X_xyz, X_xyz_from_general)
+    Y = X_xyz[22:end, :, :, :, :]
+    Y_from_general = X_xyz_from_general[22:end, :, :, :, :]
+
+    Y_zero_counts = 0
+    Y_not_found = 0
+    for n = 1:21, m = 1:2, ns = 1:4, nt = 1:4, nu = 1:4
+        diff = +Inf
+        map = nothing
+        # Search for the minimum difference,
+        # hoping thus to find the corresponding value
+        # (assuming it's a "mapping" problem)
+        for n_ = 1:21, m_ = 1:2, ns_ = 1:4, nt_ = 1:4, nu_ = 1:4
+            d = abs(Y[n, m, ns, nt, nu] - Y_from_general[n_, m_, ns_, nt_, nu_])
+            if d < diff
+                map = (n_, m_, ns_, nt_, nu_)
+                diff = d
+            end
+        end
+        v = abs(Y[n, m, ns, nt, nu])
+        n_, m_, ns_, nt_, nu_ = map
+        if Y[n, m, ns, nt, nu] == 0 && Y_from_general[n_, m_, ns_, nt_, nu_] == 0
+            global Y_zero_counts += 1
+        else
+            if (diff / v < 1.0e-6) && ((n, m, ns, nt, nu) != map)
+                print("$n $m $ns $nt $nu => $n_ $m_ $ns_ $nt_ $nu_  ")
+                print("$(Y[n,m,ns,nt,nu]) ≈ $(Y_from_general[n_,m_,ns_,nt_,nu_])")
+                if m != m_ || ns != ns_ || nt != nt_ || nu != nu_
+                    print(" Special case")
+                end
+                print("\n")
+            else
+                # println("$n $m $ns $nt $nu => not found")
+                global Y_not_found += 1
+            end
+        end
+
+    end
+
+    println("zero counts: $Y_zero_counts")
+    println("not found: $Y_not_found")
+end
+
+compare_Y_detailed(X_xyz, X_xyz_from_general)
